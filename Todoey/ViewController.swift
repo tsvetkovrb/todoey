69a52a60f3d85741b433f009cc16de9e44934f2a
Roman Tsvetkov <tsvetkovv22@gmail.com>
2020-05-06 21:25:51 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Роман Цветков on 06.05.2020.
+//  Copyright © 2020 Roman Tsvetkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
